d506b11eb226fb95ee99d7e3fbb79d22caea552c
Quentin <quentin.quero22@gmail.com>
2023-02-14 09:21:04 +0100

    test

--- src/test/NombreRomainTest.java
+++ src/test/NombreRomainTest.java
@@ -11,7 +11,7 @@ class NombreRomainsTest {
     @Test
     @DisplayName("ETANT DONNE un chiffre arabe 1" +
             "QUAND on le convertit en nombre romain" +
-            "ALORS on obtient un I")
+            "ALORS on obtient un I.")
     public void Test_1() {
         //ETANT DONNE un chiffre arabe 1
         int chiffreArabe = 1;
